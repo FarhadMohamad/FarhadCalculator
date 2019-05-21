ae0ce0c4dbe76fb3485885fdee5bb37f1379b276
FarhadMohamad <artsajmir@gmail.com>
2019-05-21 11:44:36 +0200

    FirstChanges

---
+++ SharedCalculator/CalculationOperations.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedCalculator
+{
+   public class CalculationOperations
+    {
+        //private int plus = 3 + 4;
+        //private int minus = 5 - 3;
+        //private int multiply = 5 * 2;
+        //private int divide = 10 / 2;
+
+         
+        public int plus(int a, int b)
+        {
+
+            return a + b;
+
+
+        }
+
+       
+        public int minus(int a, int b)
+        {
+
+            return a - b;
+
+
+        }
+
+        public int multiply(int a, int b)
+        {
+
+            return a * b;
+
+
+        }
+
+        public int divide(int a, int b)
+        {
+
+            return a / b;
+
+
+        }
+    }
+}
---
+++ UnitTestCalculation/UnitTest1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SharedCalculator;
+
+namespace UnitTestCalculation
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        /// <summary>
+        /// Vi opretter objekt af CalculationOperations klasse kun en gang (vi kan bruge den til alle metoder nedenfor, så behøver vi ikke at oprette den flere gange i hver metod) 
+        /// i metoden nedenfor og vi skriver også [TestInitialize]
+        /// </summary>
+        /// 
+        private CalculationOperations c;
+
+        [TestInitialize]
+        public void Setup()
+        {
+
+            c = new CalculationOperations();
+
+        }
+        [TestMethod]
+        public void TestMethodPlus()
+        {
+
+            int p = c.plus(5, 5);
+            Assert.AreEqual(10, p);
+
+        }
+
+        [TestMethod]
+        public void TestMethodMinus()
+        {
+            
+
+            int m = c.minus(5, 4);
+            Assert.AreEqual(1, m);
+        }
+        [TestMethod]
+        public void TestMethodDivide()
+        {
+
+            int d = c.divide(10, 2);
+            Assert.AreEqual(5, d);
+        }
+        [TestMethod]
+        public void TestMethodMultiply()
+        {
+
+            int m = c.multiply(5, 4);
+            Assert.AreEqual(20, m);
+        }
+    }
+}
